1a8e58d45dd45210a35ca1aa2799f50f8871e695
SilvanaPincheira <letivanapaz@gmail.com>
2025-09-29 18:03:10 -0300

    JKHY

--- app/api/send-email/route.ts
+++ app/api/send-email/route.ts
@@ -9,7 +9,7 @@ export async function POST(req: Request) {
 
     const data = await resend.emails.send({
       from: "silvana.pincheira@spartan.cl", // 👈 luego cambiaremos a sac@spartan.cl
-      to: [to],
+      to: to,
       subject,
       html: `<div style="font-family: sans-serif; font-size: 14px; color: #333;">
               ${message}
