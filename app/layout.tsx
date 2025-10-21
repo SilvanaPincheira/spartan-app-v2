82b1eea74baaa598f4bd98c001ab6e06ef96b9a7
SilvanaPincheira <letivanapaz@gmail.com>
2025-10-21 11:56:48 -0300

    button

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,6 @@
 
 import { useState } from "react";
 import { Card, CardContent } from "@/components/ui/card";
-import { Button } from "@/components/ui/button";
 import { BarChart, Bar, XAxis, Tooltip, ResponsiveContainer } from "recharts";
 
 const topVendedores = [
