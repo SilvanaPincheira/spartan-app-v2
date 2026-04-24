b919f9170ef2925e18a9f94e6c8b7798fbf4437b
SilvanaPincheira <letivanapaz@gmail.com>
2026-04-24 13:52:19 -0400

    middleware

--- app/middleware.ts
+++ app/middleware.ts
@@ -1,4 +1,5 @@
-import type { NextRequest } from "next/server";
+import { NextResponse } from 'next/server';
+import type { NextRequest } from 'next/server';
 import { createMiddlewareClient } from "@supabase/auth-helpers-nextjs";
 
 export async function middleware(req: NextRequest) {
