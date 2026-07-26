97534757be8952f6c6a853f89f5d57e96048223a
null <null>
2026-07-26 18:39:48 -0400

    precios

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,7 @@ import { usePathname } from "next/navigation";
 import { useState, useEffect, useMemo } from "react";
 import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
 import AvisoFlotante from "@/app/components/AvisoFlotante";
-import BadgePreciosVencidos from "@/app/components/BadgePreciosVencidos";
+import BadgePreciosVencidos from "@/app/api/components/BadgePreciosVencidos";
 
 
 // 🧩 Importa los módulos de modo offline
